987501d8cde3ff931dfaab35433083a03546542d
adadesions <ada_dev@icloud.com>
2015-12-30 06:19:08 +0700

    finis ads feature

--- client/helper/newsfeed.js
+++ client/helper/newsfeed.js
@@ -1,5 +1,18 @@
 Template.newsfeed.helpers({
   getAdsLimit:function (num) {
-    return Ads.find({},{limit: parseInt(num)})
+    let adsList = Ads.find().fetch(),
+        ranIndex1 = Math.floor(Math.random()*adsList.length),
+        ranIndex2 = Math.floor(Math.random()*adsList.length),
+        presentAds = []
+
+    while(true){
+      if(ranIndex1 === ranIndex2){
+        ranIndex2 = Math.floor(Math.random()*adsList.length)
+      }
+      else break
+    }
+    presentAds.push(adsList[ranIndex1])
+    presentAds.push(adsList[ranIndex2])
+    return presentAds
   }
 })
