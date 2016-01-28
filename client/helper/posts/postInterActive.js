802ac1152afdd15c3b4c1b9f1eb00f31834ad6b6
adadesions <ada_dev@icloud.com>
2016-01-28 19:02:41 +0700

    add new buttons to postFeed

--- client/helper/posts/postInterActive.js
+++ client/helper/posts/postInterActive.js
@@ -6,17 +6,24 @@ Template.likeBtn.events({
 })
 Template.likeBtn.helpers({
   isLiked: function(postId){
-      return Posts.find({_id:postId, likes:Meteor.userId()}).count() > 0 ? 'red' : ''
+      return Posts.find({_id:postId, likes:Meteor.userId()}).count() > 0 ? '/images/icon/like-icon.png' : '/images/icon/like-icon-w.png'
   }
 })
 
-// Template.commentBtn.events({
-//   'click .comment-icon': function (e) {
-//     let sel = '#post-'+$(e.target).attr('id'),
-//         $ele = $(sel).children('.feed').children('.event')
-//     if($ele.hasClass('show-comment'))
-//       $ele.addClass('hidden').removeClass('show-comment')
-//     else
-//       $ele.removeClass('hidden').addClass('show-comment')
-//   }
-// })
+Template.commentBtn.helpers({
+  isComment: function (postId) {
+      return Posts.find({_id:postId, 'comments.commentOwnerId':Meteor.userId()}).count() > 0 ? '/images/icon/comment-icon.png' : '/images/icon/comment-icon-w.png'
+  }
+})
+
+Template.favoriteBtn.helpers({
+  isFavorite: function (postId) {
+      return Posts.find({_id:postId, favorites:Meteor.userId()}).count() > 0 ? '/images/icon/favorite-icon.png' : '/images/icon/favorite-icon-w.png'
+  }
+})
+
+Template.shareBtn.helpers({
+  isShare: function (postId) {
+    return Posts.find({_id:postId, shares:Meteor.userId()}).count() > 0 ? '/images/icon/share-icon.png' : '/images/icon/share-icon-w.png'
+  }
+})
