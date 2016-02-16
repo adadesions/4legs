763bc23fe7f1b0a71d06face1f8f150ee80873b8
augisticz <augisticz@gmail.com>
2016-02-16 16:37:37 +0700

    Insert popup in forget password and button like

--- client/helper/landingPage.js
+++ client/helper/landingPage.js
@@ -4,6 +4,13 @@ Template.landingPage.onRendered(function () {
     popup : $('.login-landing.popup'),
     on    : 'click',
     position : 'bottom left'
+  }),
+
+  $('.forget-password')
+  .popup({
+    popup : $('.forget-password-landing.popup'),
+    on    : 'click',
+    position : 'top left'
   })
 
 })
@@ -26,5 +33,23 @@ Template.landingPage.events({
 
   'keypress .login-landing-form': function (e) {
     e.keyCode === 13 ? $('#loginLandingButton').trigger('click') : ''
+  },
+
+  'click #forgetPasswordLandingButton':function (e) {
+    e.preventDefault();
+    let loginId = $('[name=usernameForget]').val();
+    let emailId = $('[name=emailForget]').val();
+    let users = Meteor.users.find().fetch()
+    console.log(users)
+    users.map( u => {
+      if (loginId === u.username) {
+        Accounts.forgotPassword(emailId)
+        console.log(check)
+      }else{
+        console.log(u.username)
+        toastr.error("Your Username or Email is incorrect")
+        throw new Meteor.Error("no data");
+      }
+    })
   }
 })
