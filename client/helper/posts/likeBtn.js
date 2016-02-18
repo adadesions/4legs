cdef0b830229e5312c69c0d516942a9363b38b17
adadesions <ada_dev@icloud.com>
2016-02-18 21:25:55 +0700

    cut somthing out

--- client/helper/posts/likeBtn.js
+++ client/helper/posts/likeBtn.js
@@ -1,10 +1,26 @@
 Template.likeBtn.onRendered(function () {
-
-  $('.like-hover')
-  .popup({
-    popup : $('.popup-like.popup'),
-    target: '.like-hover',
-    position : 'top left'
-  })
-
+  // let theId = $('.like-hover').attr('id')
+  // $('.like-hover').popup({
+  //   inline: true,
+  //   target: '.like-hover',
+  //   position : 'top left'
+  // })
 })
+
+Template.likeBtn.helpers({
+  queryLiked: function (likeArray) {
+    let users = likeArray.map( id => Meteor.users.findOne({_id:id}).username)
+    return users
+  }
+})
+
+// Template.likeBtn.events({
+//   'mouseenter .like-hover': function (e) {
+//     $('.like-hover').popup({
+//       inline: true,
+//       target: '.like-hover',
+//       position : 'top left'
+//     }).popup('show')
+//
+//   }
+// })
