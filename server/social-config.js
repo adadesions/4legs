98b04da7785769949e14725f055b7b094ab4112d
adadesions <ada_dev@icloud.com>
2016-03-17 15:08:26 +0700

    update facebook login

--- server/social-config.js
+++ server/social-config.js
@@ -4,8 +4,8 @@ ServiceConfiguration.configurations.remove({
 
 ServiceConfiguration.configurations.insert({
     service: 'facebook',
-    appId: '1650233041930695',
-    secret: '30e0f9fb8f8971561dd5a8f229b749b4'    
+    appId: '1650232795264053',
+    secret: 'b121f8fd0cdd123defe16f5050d55a9b'
 });
 
 // b121f8fd0cdd123defe16f5050d55a9b
