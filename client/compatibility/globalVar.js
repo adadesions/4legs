49e5f933c2291f98d26059c2acf36790ee751707
adadesions <ada_dev@icloud.com>
2016-03-27 19:26:43 +0700

    update blank and sliver badge

--- client/compatibility/globalVar.js
+++ client/compatibility/globalVar.js
@@ -3,49 +3,55 @@ const petImgs = {
     label: 'dog',
     passiveUrl: '/images/object/6-topics/dog-gray.png',
     activeUrl: '/images/object/6-topics/dog-color.png',
-    blankUrl : '/images/object/7-profile/badges/badge-02.png',
+    sliverUrl : '/images/object/7-profile/badges/badge-02.png',
     bronzeUrl: '/images/object/7-profile/badges/badge-01.png',
-    goldUrl: '/images/object/7-profile/badges/badge-03.png'
+    goldUrl: '/images/object/7-profile/badges/badge-03.png',
+    blankUrl: '/images/object/7-profile/badges/badge-blank-dog.png'
   },
   cat : {
     label: 'cat',
     passiveUrl: '/images/object/6-topics/cat-gray.png',
     activeUrl: '/images/object/6-topics/cat-color.png',
-    blankUrl : '/images/object/7-profile/badges/badge-05.png',
+    sliverUrl : '/images/object/7-profile/badges/badge-05.png',
     bronzeUrl: '/images/object/7-profile/badges/badge-04.png',
-    goldUrl: '/images/object/7-profile/badges/badge-06.png'
+    goldUrl: '/images/object/7-profile/badges/badge-06.png',
+    blankUrl: '/images/object/7-profile/badges/badge-blank-cat.png'
   },
   pocket : {
     label: 'pocket',
     passiveUrl: '/images/object/6-topics/pocket-gray.png',
     activeUrl: '/images/object/6-topics/pocket-color.png',
-    blankUrl : '/images/object/7-profile/badges/badge-08.png',
+    sliverUrl : '/images/object/7-profile/badges/badge-08.png',
     bronzeUrl: '/images/object/7-profile/badges/badge-07.png',
-    goldUrl: '/images/object/7-profile/badges/badge-09.png'
+    goldUrl: '/images/object/7-profile/badges/badge-09.png',
+    blankUrl: '/images/object/7-profile/badges/badge-blank-pocket.png'
   },
   turtle : {
     label: 'turtle',
     passiveUrl: '/images/object/6-topics/turtle-gray.png',
     activeUrl: '/images/object/6-topics/turtle-color.png',
-    blankUrl : '/images/object/7-profile/badges/badge-14.png',
+    sliverUrl : '/images/object/7-profile/badges/badge-14.png',
     bronzeUrl: '/images/object/7-profile/badges/badge-13.png',
-    goldUrl: '/images/object/7-profile/badges/badge-15.png'
+    goldUrl: '/images/object/7-profile/badges/badge-15.png',
+    blankUrl: '/images/object/7-profile/badges/badge-blank-turtle.png'
   },
   fish : {
     label: 'fish',
     passiveUrl: '/images/object/6-topics/fish-gray.png',
     activeUrl: '/images/object/6-topics/fish-color.png',
-    blankUrl : '/images/object/7-profile/badges/badge-17.png',
+    sliverUrl : '/images/object/7-profile/badges/badge-17.png',
     bronzeUrl: '/images/object/7-profile/badges/badge-16.png',
-    goldUrl: '/images/object/7-profile/badges/badge-18.png'
+    goldUrl: '/images/object/7-profile/badges/badge-18.png',
+    blankUrl: '/images/object/7-profile/badges/badge-blank-fish.png'
   },
   bird : {
     label: 'bird',
     passiveUrl: '/images/object/6-topics/bird-gray.png',
     activeUrl: '/images/object/6-topics/bird-color.png',
-    blankUrl : '/images/object/7-profile/badges/badge-11.png',
+    sliverUrl : '/images/object/7-profile/badges/badge-11.png',
     bronzeUrl: '/images/object/7-profile/badges/badge-10.png',
-    goldUrl: '/images/object/7-profile/badges/badge-12.png'
+    goldUrl: '/images/object/7-profile/badges/badge-12.png',
+    blankUrl: '/images/object/7-profile/badges/badge-blank-bird.png'
   },
   other : {
     label: 'other',
@@ -57,15 +63,18 @@ const petImgs = {
 
 const activityBadge = {
   checkin: {
-    blankUrl: '/images/object/7-profile/badges/badge-30.png',
+    blankUrl: '/images/object/7-profile/badges/badge-blank-unknown.png',
     goldUrl: '/images/object/7-profile/badges/badge-19.png'
   },
   sos: {
-    blankUrl: '/images/object/7-profile/badges/badge-23.png',
-    goldUrl: '/images/object/7-profile/badges/badge-22.png'
+    bronzeUrl: '/images/object/7-profile/badges/badge-22.png',
+    sliverUrl: '/images/object/7-profile/badges/badge-23.png',
+    goldUrl: '/images/object/7-profile/badges/badge-24.png',
+    blankUrl: '/images/object/7-profile/badges/badge-blank-sos.png'
   },
   newLocation: {
-    blankUrl: '/images/object/7-profile/badges/badge-26.png',
-    goldUrl: '/images/object/7-profile/badges/badge-28.png'
+    sliverUrl: '/images/object/7-profile/badges/badge-26.png',
+    goldUrl: '/images/object/7-profile/badges/badge-28.png',
+    blankUrl: '/images/object/7-profile/badges/badge-blank-location.png'
   }
 }
